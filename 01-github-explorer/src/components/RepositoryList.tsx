c8348211d4c4a54e344748054390cd9795580ae4
LeandroSannt <lsn_slim@yahoo.com.br>
2021-04-29 11:38:56 -0300

    concluido

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -2,11 +2,17 @@ import {useState,useEffect} from 'react'
 import {RepositoryItem} from "./RepositoryItem";
 import '../styles/repositories.scss'
 
+interface Repository{
+    name:string;
+    description:string;
+    html_url:string;
+}
+
 
 export function RepositoryList(){
-    const [repositories,setRepositories] = useState([]);
+    const [repositories,setRepositories] = useState<Repository[]>([]);
     useEffect(() =>{
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch('https://api.github.com/orgs/rocketseat/repos')
         .then(response => response.json())
         .then(data =>setRepositories(data))
     }, [])
@@ -16,7 +22,7 @@ export function RepositoryList(){
             <h1>Lista de repositorios</h1>
             <ul>
                 {repositories.map(repository =>{
-                    return <RepositoryItem key={repository.userId} repository={repository}/>
+                    return <RepositoryItem key={repository.name} repository={repository}/>
                 })}
             </ul>
         </section>
