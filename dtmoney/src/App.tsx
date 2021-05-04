51a6e12740b616545a431431a522712161b1f79c
LeandroSannt <lsn_slim@yahoo.com.br>
2021-05-04 12:17:47 -0300

    criando modal de transações

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Modal from 'react-modal'
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root')
@@ -21,10 +22,10 @@ export function App() {
     <>
      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
      <Dashboard/>
-
-     <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}>
-            <h2>Cadastar transação</h2>
-        </Modal>
+     <NewTransactionModal
+      isOpen={isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}
+     />
      <GlobalStyle/>
     </>
   );
