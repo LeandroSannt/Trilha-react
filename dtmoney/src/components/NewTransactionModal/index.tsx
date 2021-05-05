e00a26cef9feaf3de31a04e957eca94918031ddc
LeandroSannt <lsn_slim@yahoo.com.br>
2021-05-05 00:04:42 -0300

    criando botao

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,12 +1,16 @@
 import Modal from 'react-modal'
-import { Container } from './styles'
+import { Container, RadioBox, TransactionTypeContainer } from './styles'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 import closeImg from '../../assets/close.svg'
+import { useState } from 'react'
 
 interface NewTransactionModalProps{
     isOpen:boolean;
     onRequestClose:() => void
 }
 export function NewTransactionModal({isOpen,onRequestClose}:NewTransactionModalProps){
+    const [type,setType] =useState('deposit')
 
     return(
         <Modal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName="react-modal-overlay" className="react-modal-content">
@@ -17,6 +21,18 @@ export function NewTransactionModal({isOpen,onRequestClose}:NewTransactionModalP
                 <h2>Cadastar transação</h2>
                 <input type="text" placeholder="Titulo"/>
                 <input type="number" placeholder="Valor"/>
+                <TransactionTypeContainer>
+                    <RadioBox type="button" onClick={() => setType('deposit')} isActive={type === 'deposit'} activeColor= 'green'>
+                        <img src={incomeImg} alt="Entrada"/>
+                        <span>Entrada</span>
+                    </RadioBox>
+
+                    <RadioBox type="button" onClick={() => setType('withdraw')} isActive={type === 'withdraw'} activeColor= 'red'>
+                        <img src={outcomeImg} alt="Saida"/>
+                        <span>Saida</span>
+                    </RadioBox>
+
+                </TransactionTypeContainer>
                 <input type="number" placeholder="Categoria"/>
                 <button type="submit">Cadastar</button>
             </Container>
